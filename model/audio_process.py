8760dfcfc80eb0d6b969148fa2258e041fd038cc
Daniel Semeniuta <dsemeniu@stanford.edu>
2019-03-16 00:38:58 -0700

    predict fixes

--- model/audio_process.py
+++ model/audio_process.py
@@ -6,6 +6,7 @@ import numpy as np
 import librosa
 
 from scipy.signal import decimate
+from scipy import interpolate
 from matplotlib import pyplot as plt
 
 import torch
@@ -24,18 +25,28 @@ def save_spectogram(S, outfile='spectrogram.png'):
     plt.savefig(outfile)
 
 def upsample_wav(wav, args, model, out_dir):
-    """ Adapted from kuleshov/audio-super-res """
+    """ Adapted from kuleshov/audio-super-res
+    Args:
+        wav (string) : location of wav file to upsample
+        args (args dict) : parameters of run
+        model (nn.Module) : pytorch module
+        out_dir (string) : where to write the predictions and results
+    """
     # load signal
     x_hr, fs = librosa.load(wav, sr=args.sr) # high-res
-    print(x_hr.shape)
 
     # downscale signal
     x_lr = decimate(x_hr, args.r) # low-res
-    x_lr_tensor = torch.from_numpy(x_lr.copy()).double()
-    x_lr_tensor = torch.unsqueeze(torch.unsqueeze(x_lr_tensor, 0), -1)
-    print(x_lr_tensor.shape)
+    # x_lr_tensor = torch.from_numpy(x_lr.copy()).double()
+    # x_lr_tensor = torch.unsqueeze(torch.unsqueeze(x_lr_tensor, 0), -1)
     # upscale the low-res version
-    out = model.forward(x_lr_tensor)
+    # out = model_predict(x_lr, model, args.r)
+
+    # get low-res ready for model
+    x_sp = spline_up(x_lr, args.r)
+    x_sp = x_sp[:len(x_sp) - (len(x_sp) % (2**(model.B+1)))]
+    X = torch.from_numpy(x_sp.reshape((1,len(x_sp),1)).copy())
+    out = model.forward(X)
     x_pr = np.array(out.squeeze()) # pred
 
     # crop so that it works with scaling ratio
@@ -55,3 +66,24 @@ def upsample_wav(wav, args, model, out_dir):
     save_spectogram(S, outfile=outname + '.hr.png')
     S = get_spectogram(x_lr, n_fft=2048/args.r)
     save_spectogram(S, outfile=outname + '.lr.png')
+
+def spline_up(x_lr, r):
+    """ adapted from kuleshov/audio-super-res
+    Performs a cubic spline on an input so that it can be fed into the model
+
+    Args:
+        x_lr (np.array) : low res x input
+        r (int) : upscaling ratio
+    """
+    x_lr = x_lr.squeeze()
+    x_hr_len = len(x_lr) * r
+    x_sp = np.zeros(x_hr_len)
+
+    i_lr = np.arange(x_hr_len, step=r)
+    i_hr = np.arange(x_hr_len)
+
+    f = interpolate.splrep(i_lr, x_lr)
+
+    x_sp = interpolate.splev(i_hr, f)
+
+    return x_sp
